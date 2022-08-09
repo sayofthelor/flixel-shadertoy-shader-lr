bf4232330c9052e6f97b2a253dcda89fbc007f53
sayofthelor <justsailor06@gmail.com>
2022-08-09 08:32:55 -0400

    Update ShaderToySprite.hx

--- src/shadertoy/ShaderToySprite.hx
+++ src/shadertoy/ShaderToySprite.hx
@@ -3,23 +3,32 @@ package shadertoy;
 import flixel.FlxSprite;
 import flixel.FlxG;
 import openfl.filters.ShaderFilter;
+import flixel.FlxCamera;
+import flixel.math.FlxPoint;
 
 class ShaderToySprite extends FlxSprite {
-    public var shadertoy:FlxShaderToyShader = null;
-    var flt:FlxFilterFrames = null;
+    public var cam:FlxCamera = null;
+    public var shaderToy:FlxShaderToyShader = null;
+    override public function new(x:Float = 0, y:Float = 0, ?SimpleGraphic:Null<flixel.system.FlxGraphicAsset>) {
+        super(x, y, SimpleGraphic);
+        cam = new FlxCamera(width, height);
+    }
     override public function update(elapsed:Float):Void {
-        if (shadertoy != null) {
-            shadertoy.update(elapsed, FlxG.mouse);
-            pixels.applyFilter(0, 0, width, height, new ShaderFilter(shadertoy));
+        if (shaderToy != null && cam != null) {
+            shaderToy.update(elapsed, FlxG.mouse);
+            if (FlxG.render.blit)
+                pixels.copyPixels(cam.buffer, cam.rect, new FlxPoint());
+            else
+                pixels.draw(cam.canvas);
         }
         super.update(elapsed);
     }
-    public function setShader(shader:Null<FlxShaderToyShader> = null):Void {
-        if (shader == null) return;
-        shadertoy = shader;
-        shadertoy.iResolution.value = [width, height];
+    public function updateCam():Void {
+        cam.width = width;
+        cam.height = height;
     }
-    public function removeShader():Void {
-        shadertoy = null;
+    public function setShader(shader:Null<FlxShaderToyShader>):Void {
+        shaderToy = shader;
+        cam.setFilters([new ShaderFilter(shaderToy)]);
     }
 }
